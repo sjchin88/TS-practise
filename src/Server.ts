7a8d279fb7479287bc7ac9b7e08c6078c5ae15ed
sjchin88 <71600246+sjchin88@users.noreply.github.com>
2025-06-01 19:02:58 -0700

    finalize

--- src/Server.ts
+++ src/Server.ts
@@ -1,11 +1,22 @@
 import express, { Request, Response } from "express";
 
+//Specify port used
+const port = 3000;
+
 export class Server {
+  // This app will get all basic functionality of express
   private app = express();
 
   startServer() {
+    // Specify the end point /hello, handle Request & Response
     this.app.get("/hello", (req: Request, res: Response) => {
       res.send("Hello");
     });
+
+    this.app.listen(port, () => {
+      console.log("Listening on port " + port);
+    });
   }
 }
+
+new Server().startServer();
